d585bb3f8f431d0f241b0a42a68455d8a3c4d7f1
Leonard Fedetov <l.fedetov1@gmail.com>
2026-06-05 08:47:35 +0000

    ok new

--- pages/POS/jsobjects/PosCashCounter/PosCashCounter.js
+++
@@ -1,9 +0,0 @@
-export default {
-  defaultLines() {
-    return [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01].map(value => ({
-      denominationValue: value,
-      quantity: 0,
-      amount: 0
-    }));
-  }
-};
